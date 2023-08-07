bcb34e52aefa23af1286510d8a15d5ea669aaff3
Elio Rocha <elioneto21@gmail.com>
2023-08-06 22:14:25 -0300

    Exercicio08

---
+++ src/main/java/exercicio01/questao09/exercicio09.java
@@ -0,0 +1,10 @@
+package main.java.exercicio01.questao09;
+
+//Encontre o quadrado dos números de 0 até 10. Utilize o controle de fluxo for
+public class exercicio09 {
+    public static void main(String[] args) {
+      for (int numero = 0; numero <=10; numero++){
+          System.out.println(numero * numero);
+      }
+    }
+}
