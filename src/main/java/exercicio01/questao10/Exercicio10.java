7001d11ad6903a323dbeb28dc2c712495578f1ce
Elio Rocha <elioneto21@gmail.com>
2023-08-06 22:28:57 -0300

    Exercicio10

---
+++ src/main/java/exercicio01/questao10/Exercicio10.java
@@ -0,0 +1,19 @@
+package main.java.exercicio01.questao10;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+
+      int a = 6;
+      int b = 4;
+      int resultado = a / b;
+
+        System.out.println(resultado);
+
+        /*O valor da variável foi 1 pois as variáveis foram declaradas como inteiras, e não real. Portanto, a divisão
+        de numeros inteiros resulta em números também inteiros.*/
+
+    }
+}
